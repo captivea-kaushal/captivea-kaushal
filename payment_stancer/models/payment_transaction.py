d83ee48d135442039c1553ffdf27bf507a95a1a7
captivea-kaushal <kaushal.dhamelia@captivea.com>
2024-02-14 19:19:02 +0530

    [ADD] complete refund operation  v16

--- payment_stancer/models/payment_transaction.py
+++ payment_stancer/models/payment_transaction.py
@@ -1,12 +1,10 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import logging
-
-from werkzeug.urls import url_join
 from werkzeug import urls
-
-from odoo import _, models
-
+from odoo import _, models, fields
+import requests
+import base64
 from odoo.addons.payment import utils as payment_utils
 from odoo.addons.payment_stancer.controllers.main import StancerController
 from odoo.exceptions import ValidationError
@@ -17,6 +15,13 @@ _logger = logging.getLogger(__name__)
 
 class PaymentTransaction(models.Model):
     _inherit = 'payment.transaction'
+
+    refund_response = fields.Text(string='Refund Response', translate=True, readonly=True)
+    refund_processed = fields.Boolean(string='Refund Done', readonly=True)
+    stancer_refund_id = fields.Char(string='Stancer Refund Id', readonly=True)
+    is_refund_transfer = fields.Boolean(string='Is Refund', readonly=True)
+    stancer_refund_tx_id = fields.Many2one(comodel_name='payment.transaction', string='Stancer Refund Transaction',
+                                           readonly=True)
 
     def _get_specific_rendering_values(self, processing_values):
         """ Override of payment to return Stancer rendering values.
@@ -31,7 +36,7 @@ class PaymentTransaction(models.Model):
 
         _logger.warning(processing_values)
         # Initiate the payment and retrieve the payment link data.
-        base_url = 'https://d186-2405-201-200f-18b4-9b5b-74f8-cdc2-8398.ngrok-free.app/' #self.provider_id.get_base_url()
+        base_url = 'https://eef0-2405-201-200f-18b4-e8b3-3f07-271a-3752.ngrok-free.app/' #self.provider_id.get_base_url()
 
         payload = {
             'order_id': self.reference,
@@ -102,3 +107,62 @@ class PaymentTransaction(models.Model):
                   "%s.") % reference)
         _logger.warning(tx)
         return tx
+
+    def action_stancer_refund(self):
+        stancer_provider = self.provider_id
+        payment_method_line = stancer_provider.journal_id.inbound_payment_method_line_ids \
+            .filtered(lambda l: l.code == stancer_provider.code)
+        amount = self.amount * 100
+        stancer_payment_id = self.provider_reference
+
+        token = base64.b64encode(f"{stancer_provider.stancer_key_secret}:{''}".encode('utf-8')).decode("ascii")
+
+        headers = {
+            'Content - Type': 'application / json',
+            'Authorization': f'Basic {token}'
+        }
+        payload = {
+            'payment': stancer_payment_id,
+            'amount': amount
+        }
+
+        request_url = 'https://api.stancer.com/v1/refunds/'
+        refund_request = requests.post(request_url, json=payload, headers=headers)
+        refund_response = refund_request.json()
+        if 'id' in refund_response:
+            self.stancer_refund_id = refund_response.get('id')
+            self.refund_processed = True
+            self.refund_response = str(refund_response)
+            refund_tx = self.env['payment.transaction'].create({
+                'provider_id': stancer_provider.id,
+                'partner_id': self.partner_id.id,
+                'reference': self.reference + ' Stancer Refund',
+                'amount': -amount / 100,
+                'state': 'done',
+                'currency_id': self.currency_id.id,
+                'provider_reference': refund_response.get('payment'),
+                'stancer_refund_id': refund_response.get('id'),
+                'is_refund_transfer': True,
+                'refund_response': refund_response.get('status')
+            })
+            self.stancer_refund_tx_id = refund_tx.id
+            payment_values = {
+                'amount': amount / 100,
+                'payment_type': 'outbound',
+                'currency_id': self.currency_id.id,
+                'partner_id': self.partner_id.commercial_partner_id.id,
+                'partner_type': 'customer',
+                'journal_id': stancer_provider.journal_id.id,
+                'company_id': stancer_provider.company_id.id,
+                'payment_method_line_id': payment_method_line.id,
+                'payment_transaction_id': refund_tx.id,
+                'ref': self.reference + ' Stancer Refund'
+            }
+
+            payment = self.env['account.payment'].sudo().create(payment_values)
+            refund_tx.payment_id = payment.id
+            payment.action_post()
+
+        else:
+            raise ValidationError(_('Request issue occurred.\n Error Response : %s', str(refund_response)))
+
